acee2d3123a6d94792d15728d345a47afb6c142f
Wilhelm Shen <33245687+WilhelmIllust@users.noreply.github.com>
2020-10-20 16:31:10 +0800

    first commit

--- 00757128_hw2/ContentView.swift
+++ 00757128_hw2/ContentView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            homePage()
+                .tabItem {
+                    Image(systemName: "house")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                    Text("Home")
+                }
+            tourGuide()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                    Text("Tour Guide")
+                }
+            gallery()
+                .tabItem {Image(systemName: "square")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                    Text("Gallery")
+                }
+            changeIcon()
+                .tabItem {Image(systemName: "square")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                    Text("App Icon")
+                }
+        }
     }
 }
 
